9968e1c50b1ea3d52e0c48bf6fefdbdb0cb515c2
Mateuszfa <Matek.falak@gmail.com>
2021-12-18 14:13:05 +0000

    update v3.0

--- testy.py
+++ testy.py
@@ -1,3 +1,29 @@
+world  = input('[u]sa czy [e]uropa?: ')
+if world =='u':
+    print("witaj w USA! ")
+if world =='e':
+    print("Witaj w Europie! ")
+
+sex = input('[k]obieta czy [m]ezczyzna ?:')
+if sex == 'k':
+    print('mozesz uzywac aplikacje Droga Pani')
+    wiek = input("Podaj wiek: ")
+    # Sprawdzamy, czy wiek jest złożony z cyfr
+    if wiek.isdigit() == False:
+        exit("Wiek musi być liczbą")
+    wiek = int(wiek)
+    if wiek >= 18 and wiek <= 65:
+        print("Witamy w aplikacji i smacznej degustacji!")
+    elif wiek > 65:
+        print("Jesteś już za stara na alkohol")
+    else:
+        exit('jesteś za młoda!')
+elif sex =='m':
+    print('Ta aplikacja nie jest dla facetow, sorry!')
+    exit()
+
+
+
 sex = input('[k]obieta czy [m]ezczyzna ?:')
 if sex == 'k':
     print('mozesz uzywac aplikacje Droga Pani')
@@ -9,11 +35,13 @@ if sex == 'k':
     if wiek >= 21 and wiek <= 65:
         print("Witamy w aplikacji i smacznej degustacji!")
     elif wiek > 65:
-        print("Jesteś już za stary/a na alkohol")
+        print("Jesteś już za stara na alkohol")
     else:
-        exit('jesteś za młody/a!, w USA wymagamy 21 lat')
+        exit('jesteś za młoda!, w USA wymagamy 21 lat')
 elif sex =='m':
     print('Ta aplikacja nie jest dla facetow, sorry!')
+    exit()
 
 else:
     print('Wybrano nieoslugiwana wartosci. Aplikacja jest tylko dla pelnoletnich kobiet. Sprobuj ponownie')
+
