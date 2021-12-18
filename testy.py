567a3e43a4b2ea13f482207fa38f7443b0c96799
Mateuszfa <Matek.falak@gmail.com>
2021-12-18 15:16:01 +0000

    update v3.0

--- testy.py
+++ testy.py
@@ -2,41 +2,42 @@
 world  = input('[u]sa czy [e]uropa?: ')
 if world =='u':
     print("witaj w USA! ")
+
+
+sex = input('[k]obieta czy [m]ezczyzna ?:')
+if sex == 'k':
+    print('mozesz uzywac aplikacje Droga Pani')
+    wiek = input("Podaj wiek: ")
+    # Sprawdzamy, czy wiek jest złożony z cyfr
+if wiek.isdigit() == False:
+    exit("Wiek musi być liczbą")
+    wiek = int(wiek)
+if wiek >= 21 and wiek <= 65:
+        print("Witamy w aplikacji i smacznej degustacji!")
+
+elif wiek > 65:
+        print("Jesteś już za stara na alkohol")
+else:
+    exit('jesteś za młoda!')
+if sex =='m':
+    exit('Ta aplikacja nie jest dla facetow, sorry!')
 if world =='e':
     print("Witaj w Europie! ")
-
 sex = input('[k]obieta czy [m]ezczyzna ?:')
 if sex == 'k':
     print('mozesz uzywac aplikacje Droga Pani')
-    wiek = input("Podaj wiek: ")
-    # Sprawdzamy, czy wiek jest złożony z cyfr
-    if wiek.isdigit() == False:
-        exit("Wiek musi być liczbą")
+wiek = input("Podaj wiek: ")
+# Sprawdzamy, czy wiek jest złożony z cyfr
+if wiek.isdigit() == False:
+    exit("Wiek musi być liczbą")
     wiek = int(wiek)
-    if wiek >= 18 and wiek <= 65:
-        print("Witamy w aplikacji i smacznej degustacji!")
-        exit()
-    elif wiek > 65:
+if wiek >= 18 and wiek <= 65:
+    print("Witamy w aplikacji i smacznej degustacji!")
+else:
+    exit('jesteś za młoda!')
+if wiek > 65:
         print("Jesteś już za stara na alkohol")
-    else:
-        exit('jesteś za młoda!')
-elif sex =='m':
-    exit('Ta aplikacja nie jest dla facetow, sorry!')
 
-sex = input('[k]obieta czy [m]ezczyzna ?:')
-if sex == 'k':
-    print('mozesz uzywac aplikacje Droga Pani')
-    wiek = input("Podaj wiek: ")
-    # Sprawdzamy, czy wiek jest złożony z cyfr
-    if wiek.isdigit() == False:
-        exit("Wiek musi być liczbą")
-    wiek = int(wiek)
-    if wiek >= 21 and wiek <= 65:
-        print("Witamy w aplikacji i smacznej degustacji!")
-    elif wiek > 65:
-        print("Jesteś już za stara na alkohol")
-    else:
-        exit('jesteś za młoda!, w USA wymagamy 21 lat')
 elif sex =='m':
     print('Ta aplikacja nie jest dla facetow, sorry!')
     exit()
